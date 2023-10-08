6f94c07844aeea4919b01e5b0a42629fe027acd1
Rohit-554 <rohitkumarccc68@gmail.com>
2023-10-08 10:57:01 +0530

    Prepare OnBoardingScreens

---
+++ app/src/main/java/io/jadu/statshunter/valorant/onboardingscreens/OnBoardingScreens.kt
@@ -0,0 +1,22 @@
+package io.jadu.statshunter.valorant.onboardingscreens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.jadu.statshunter.R
+import io.jadu.statshunter.databinding.FragmentOnBoardingScreensBinding
+
+
+class OnBoardingScreens : Fragment() {
+    private lateinit var binding: FragmentOnBoardingScreensBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentOnBoardingScreensBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
--- app/src/main/java/io/jadu/statshunter/valorant/valorant_game_infos/weapons/data/data_source/remote/dto/WeaponsDataDto.kt
+++ app/src/main/java/io/jadu/statshunter/valorant/valorant_game_infos/weapons/data/data_source/remote/dto/WeaponsDataDto.kt
@@ -1,6 +1,9 @@
 package io.jadu.statshunter.valorant.valorant_game_infos.weapons.data.data_source.remote.dto
 
 import io.jadu.statshunter.valorant.valorant_game_infos.weapons.domain.model.AdsStats
+import io.jadu.statshunter.valorant.valorant_game_infos.weapons.domain.model.AirBurstStats
+import io.jadu.statshunter.valorant.valorant_game_infos.weapons.domain.model.AltShotgunStats
+import io.jadu.statshunter.valorant.valorant_game_infos.weapons.domain.model.DamageRange
 import io.jadu.statshunter.valorant.valorant_game_infos.weapons.domain.model.GridPosition
 import io.jadu.statshunter.valorant.valorant_game_infos.weapons.domain.model.ShopData
 import io.jadu.statshunter.valorant.valorant_game_infos.weapons.domain.model.WeaponStats
@@ -19,4 +22,20 @@ data class WeaponsDataDto(
     val weaponStats: WeaponStatsDto?,
 )
 
-//TODO: Add toDomainWeaponsData() function
+fun WeaponsDataDto.toDomainWeaponsData(): WeaponsData {
+    return WeaponsData(
+        assetPath = this.assetPath ?: "",
+        category = this.category ?: "",
+        defaultSkinUuid = this.defaultSkinUuid ?: "",
+        displayIcon = this.displayIcon ?: "",
+        displayName = this.displayName ?: "",
+        killStreamIcon = this.killStreamIcon ?: "",
+        shopData = this.shopData?.toDomainShopData() ?: ShopData("", false, "", "", 0, GridPosition(0,0), "", "", ""),
+        skins = this.skins?.map { it.toDomainSkin() } ?: emptyList(),
+        uuid = this.uuid ?: "",
+        weaponStats = this.weaponStats?.toDomainWeaponStats() ?: WeaponStats(
+            AdsStats(0,0.0,0.0,0.0,0.0),
+            AirBurstStats(0.0,0),
+            "", AltShotgunStats(0.0,0), emptyList(), 0.0, "", "", 0.0, 0.0, 0,0.0,0.0,0,"")
+    )
+}
